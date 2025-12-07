c1945843a9b1d1d4ff606ae005fc7b4b1fe8afa2
MostofaKobirAdon <mostofakobiradon9@gmail.com>
2025-12-07 15:16:44 +0600

    Successfully connected to mongodb

--- index.js
+++ index.js
@@ -2,11 +2,50 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = 3000;
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // middlewares
 
 app.use(express.json());
 app.use(cors());
+
+const uri = `${process.env.MONGODB_URI}`;
+
+// client
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+//-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const db = client.db("contest_x_db");
+    const contestsCollection = db.collection("contests");
+
+    // api
+   
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+//-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
 
 app.get("/", (req, res) => {
   res.send("contest x server is running");
